9456c77e6de23204efba35c59708cb6adc8bf8e6
Bryan Jennings <bryanjenningz@gmail.com>
2016-08-15 01:08:05 -0700

    add react

--- app/index.js
+++ app/index.js
@@ -1,1 +1,10 @@
-console.log('hello');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var App = React.createClass({
+  render: function() {
+    return <div>Hello there</div>;
+  }
+});
+
+ReactDOM.render(<App />, document.querySelector('#root'));
